d0655fd3d48b593b8ce85b65b1f917062c341922
0x000011b <0x000011b@proton.me>
2023-02-28 10:29:34 -0300

    fix: enable `pgcrypto` during migration that requires UUID

--- db/migrations/20230223193937_create_chats.cr
+++ db/migrations/20230223193937_create_chats.cr
@@ -1,5 +1,7 @@
 class CreateChats::V20230223193937 < Avram::Migrator::Migration::V1
   def migrate
+    enable_extension "pgcrypto"
+
     create table_for(Chat) do
       # TODO(11b): Default this to ULID or UUIDv6 to avoid index fragmentation.
       primary_key id : UUID
@@ -14,5 +16,6 @@ class CreateChats::V20230223193937 < Avram::Migrator::Migration::V1
 
   def rollback
     drop table_for(Chat)
+    disable_extension "pgcrypto"
   end
 end
